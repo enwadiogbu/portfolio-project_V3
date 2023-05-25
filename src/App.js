f6ac58800dd2d7247facf23fea2907a3ada46c35
enwadiogbu <emekanwadiogbu.jr@me.com>
2023-05-24 19:46:41 -0700

    Added About Me component. The image is suppose to center but justify content and align items did not solve image center prompt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar />
     <Header />
+    <AboutMe />
     
     </>
   );
